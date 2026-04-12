e5be2916d7c837bd1183420d807bbfd9a0ae4398
Liang-cmd777 <draftsman369@gmail.com>
2026-04-12 10:36:13 +0200

    mongodb_database_setup-commit

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+
+
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
@@ -11,6 +17,12 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+db.on('error', error => console.log(error))
+db.once('open', () => console.log('connected to mongoose'))
 
 
 app.use('/', indexRouter)
